5c24f3e242fc699376db2437f0259de75a6ef41f
poonammandekar <mandekarpoonam8@gmail.com>
2024-12-02 18:30:54 +0530

    initial framework setup

---
+++ CSharpSelFramework/Tests/WindowHandlers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using CSharpSelFramework.utilities;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class WindowHandlers:TestBase
+    {
+        
+
+       
+
+        [Test]
+        public void WindowHandle()
+
+        {
+            String email = "mentor@rahulshettyacademy.com";
+            String parentWindowId =driver.CurrentWindowHandle;
+            driver.FindElement(By.ClassName("blinkingText")).Click();
+
+            Assert.AreEqual(2, driver.WindowHandles.Count);//1
+
+            String childWindowName = driver.WindowHandles[1];
+
+            driver.SwitchTo().Window(childWindowName);
+
+            TestContext.Progress.WriteLine(driver.FindElement(By.CssSelector(".red")).Text);
+            String text =driver.FindElement(By.CssSelector(".red")).Text;
+
+            // Please email us at mentor @rahulshettyacademy.com with below template to receive response
+
+            String[] splittedText = text.Split("at");
+
+            String[] trimmedString = splittedText[1].Trim().Split(" ");
+
+            Assert.AreEqual(email, trimmedString[0]);
+
+            driver.SwitchTo().Window(parentWindowId);
+
+            driver.FindElement(By.Id("username")).SendKeys(trimmedString[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ CSharpSelFramework/utilities/TestBase.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSharpSelFramework.utilities
+{
+    public class TestBase
+    {
+        public  IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            string browser=ConfigurationManager.AppSettings["browser"];
+            InitBrowser(browser);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        public void InitBrowser(string browserName)
+        {
+            System.Console.WriteLine("Calling Driver");
+            switch (browserName)
+            {
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "Edge":
+                    new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+                    driver = new EdgeDriver();
+                    break;
+
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Close();
+        }
+    }
+}
